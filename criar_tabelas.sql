cd09f7b0ded9134e8d401e7789dc656e7a5943d8
Lucas Garcia Resende <117584139+LucasGarciaResende@users.noreply.github.com>
2025-05-29 10:59:11 -0300

    atualizando script the criação de tabelas

--- criar_tabelas.sql
+++ criar_tabelas.sql
@@ -1,54 +1,50 @@
-
 --Criação das tabela
 
 CREATE TABLE Drivers (
-
     DriverID INT PRIMARY KEY,
-
     Nome VARCHAR(100),
-
     CNH VARCHAR(20),
-
     Endereço VARCHAR(200),
-
     Contato VARCHAR(50)
-
 );
-
- 
 
 CREATE TABLE Clients (
-
     ClientID INT PRIMARY KEY,
-
     Nome VARCHAR(100),
-
     Empresa VARCHAR(100),
-
     Endereço VARCHAR(200),
-
     Contato VARCHAR(50)
-
 );
 
- 
-
 CREATE TABLE Orders (
-
     OrderID INT PRIMARY KEY,
-
     ClientID INT,
-
     DriverID INT,
-
     DetalhesPedido TEXT,
-
     DataEntrega DATE,
-
     Status VARCHAR(50),
-
     FOREIGN KEY (ClientID) REFERENCES Clients(ClientID),
-
     FOREIGN KEY (DriverID) REFERENCES Drivers(DriverID)
 
 );
+
+CREATE SEQUENCE seq_drivers_id
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE seq_clients_id
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE seq_orders_id
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
